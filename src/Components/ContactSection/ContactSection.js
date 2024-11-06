9dba18c9d1d405bfcbc6ad691b68fe71efefc7f0
cediazz <dce92179@gmail.com>
2024-11-05 21:10:35 -0500

    ContactFormManagement-SweetAlert

--- src/Components/ContactSection/ContactSection.js
+++ src/Components/ContactSection/ContactSection.js
@@ -1,26 +1,47 @@
 import { useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import Loading from '../Loading/Loading';
+import Swal from 'sweetalert2'
 
 function ContactSection() {
 
-    const [fullName, setFullName] = useState()
-    const [email, setEmail] = useState()
-    const [phoneNumber, setPhoneNumber] = useState()
-    const [message, setMessage] = useState()
+   const [loading, setLoading] = useState(false)
 
 
     const validationSchema = Yup.object().shape({
-        fullName: Yup.string().required('El nombre es obligatorio'),
-        email: Yup.string().email('El correo electrónico no es válido').required('El correo electrónico es obligatorio'),
-        phoneNumber: Yup.string()
-        .matches(/^[0-9]{10}$/, 'El número de teléfono debe tener 10 dígitos')
-        .required('El número de teléfono es obligatorio'),
-        message: Yup.string().required('El mensaje es obligatorio'),
+        fullName: Yup.string().required('A name is required.'),
+        email: Yup.string().email('Email is not valid.').required('An email is required.'),
+        message: Yup.string().required('A message is required.'),
     })
 
     const handleSubmit = async (values) => {
+        setLoading(true)
+        values["access_key"] = "26cb8c16-33d0-472b-abf9-fabf5c9617e9"
         console.log(values)
+        const json = JSON.stringify(values);
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json());
+        if (res.success) {
+           setLoading(false)
+           Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success",
+            color: '#16A085',
+            confirmButtonColor: '#16A085'
+          });
+
+        }
+        else{
+            setLoading(false)
+        }
     }
 
 
@@ -36,42 +57,35 @@ function ContactSection() {
                 <div class="row justify-content-center">
                     <div class="col-lg-8 col-xl-7">
                         <Formik
-                            initialValues={{ fullName: '', email: '', phoneNumber:'', message:'' }}
+                            initialValues={{ fullName: '', email: '', message: '' }}
                             validationSchema={validationSchema}
-                            onSubmit={(values) => {
-                                console.log('Formulario enviado', values);
-                              }}
+                            onSubmit={(values) => handleSubmit(values)}
                         >
-                            
                             <Form>
                                 <div class="form-floating mb-3">
                                     <Field class="form-control" name="fullName" type="text" />
                                     <label for="name">Full name</label>
-                                    <ErrorMessage className="invalid-feedback" name="fullName"  />
+                                    <ErrorMessage name="fullName" component="div" className='error-message' ></ErrorMessage>
                                 </div>
                                 <div class="form-floating mb-3">
                                     <Field class="form-control" name="email" type="email" />
                                     <label for="email">Email address</label>
-                                    <ErrorMessage class="invalid-feedback" name="email"  />
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <Field class="form-control" name="phoneNumber" type="tel" />
-                                    <label for="phone">Phone number</label>
-                                    <ErrorMessage class="invalid-feedback" name="phoneNumber"  />
+                                    <ErrorMessage name="email" component="div" className='error-message' />
                                 </div>
                                 <div class="form-floating mb-3">
                                     <Field class="form-control" as="textarea" name="message" />
                                     <label for="message">Message</label>
-                                    <ErrorMessage class="invalid-feedback" name="message"  />
+                                    <ErrorMessage name="message" component="div" className='error-message' />
                                 </div>
-                                
+
                                 <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
                                 <button class="btn btn-primary btn-xl" type="submit">Send</button>
                             </Form>
-                           
+
                         </Formik>
                     </div>
                 </div>
+                {loading && <Loading />}
             </div>
         </section>
     )
---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,13 @@
+
+function Loading() {
+
+    return (
+        <div class="d-flex justify-content-center">
+            <div class="spinner-border my-color" style={{width: '3rem', height: '3rem'}} role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+
+    )
+}
+export default Loading
