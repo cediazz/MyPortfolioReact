6ec880ec5dd4b32c397a1d597e55003bcd861091
cediazz <dce92179@gmail.com>
2025-03-01 12:51:01 -0500

    ChangeUserProfileModel

--- Backend/PortfolioAPI/user_management/models.py
+++ Backend/PortfolioAPI/user_management/models.py
@@ -5,6 +5,9 @@ class UserProfile(models.Model):
     profession = models.CharField(max_length=256,verbose_name='profesión')
     skills = models.CharField(max_length=256,verbose_name='habilidades')
     about_me = models.TextField(verbose_name='acerca de mi')
+    location = models.CharField(max_length=256,verbose_name='dirección',null=True)
+    facebook_url = models.URLField(max_length=200, blank=True, null=True,verbose_name='url de facebook')
+    linkedin_url = models.URLField(max_length=200, blank=True, null=True,verbose_name='url de linkedin')
     image = models.ImageField(upload_to='user_images',blank=True,verbose_name='imagen del usuario')
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile', verbose_name='usuario')
 
@@ -13,3 +16,4 @@ class UserProfile(models.Model):
     
     class Meta:
         verbose_name = 'Perfil del usuario'
+        db_table = 'auth_user_profile'
