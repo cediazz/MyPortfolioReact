3d5e473e6d2f799034a397764f5e2f97ec07e4fa
cediazz <dce92179@gmail.com>
2025-03-02 17:32:58 -0500

    Page404Component

---
+++ Frontend/src/Components/Page404/Page404.js
@@ -0,0 +1,18 @@
+
+const Page404 = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="row text-center"> 
+                <div className="col-12"> 
+                    <h1 className="text-danger display-1">
+                        <i className="fas fa-exclamation-triangle"></i> 
+                    </h1>
+                </div>
+                <div className="col-12"> 
+                    <h1 className="text-danger display-1">404 - Page Not Found</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Page404
--- Frontend/src/context/userContext.js
+++ Frontend/src/context/userContext.js
@@ -1,11 +1,14 @@
 import { createContext, useState,useEffect } from 'react'
 import axios from 'axios'
+import Page404 from '../Components/Page404/Page404'
 
 export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
     
     const [user, setUser] = useState()
+    const [error, setError] = useState(false)
+
 
     const getUser = async () => {
         try {
@@ -14,6 +17,7 @@ export const UserProvider = ({ children }) => {
             setUser(response.data)
         } catch (error) {
           console.log(error)
+          setError(true)
         }
       }
 
@@ -23,6 +27,10 @@ export const UserProvider = ({ children }) => {
         }
         fetchData()
     },[] )
+
+    if (error){
+       return <Page404 />
+    }
 
     return (
         <UserContext.Provider value={{user}}>
